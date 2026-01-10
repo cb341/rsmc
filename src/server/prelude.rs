ebae9168e71add2b0e7a4197196a7028541331e9
Daniel Bengl <daniel.bengl@renuo.ch>
2026-01-10 22:09:54 +0100

    Add previously missing resource import

--- src/server/prelude.rs
+++ src/server/prelude.rs
@@ -7,6 +7,7 @@ pub use std::time::SystemTime;
 pub use bevy::app::{App, Plugin, Startup, Update};
 pub use bevy::ecs::event::*;
 pub use bevy::ecs::system::*;
+pub use bevy::ecs::resource::*;
 pub use bevy::log::{debug, error, info, warn};
 pub use bevy::math::*;
 pub use bevy::MinimalPlugins;
